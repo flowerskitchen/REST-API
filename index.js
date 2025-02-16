7264ff77c992b75b5d71c3b9fddefec75a60cbb8
flowerskitchen <flowerskitchen4@gmail.com>
2025-02-16 20:46:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 
+
+
+
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
